2cd4e9517d3405d18ed677d1ab1573466f819664
chockalingam <enanyang.dev@virinchisoftware.com>
2022-06-16 12:09:07 +0530

    style updates

---
+++ src/data/questions.js
@@ -0,0 +1,17 @@
+export const questionData = [
+    {
+        id: "q1",
+        question: "What is React.js?",
+        answer: "React is an JavaScript Libray used for building user interfaces" 
+    },
+    {
+        id: "q2",
+        question: "What is Jsx?",
+        answer: "React is an JavaScript Libray used for building user interfaces" 
+    },
+    {
+        id: "q3",
+        question: "What is VirtualDOM",
+        answer: "React is an JavaScript Libray used for building user interfaces" 
+    },
+]
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import Layout from '../../components/Layout';
+import Accordion from '../../components/Accordion';
 
 const Login = () => {
   return (
-    <Layout>From Login</Layout>
+    <Layout>
+        <Accordion />
+    </Layout>
   )
 }
 
